72b639237f3b7ec1a7338fe0a9adf0afc6f730a7
sdayneko <sergeydayneko@mail.ru>
2020-03-12 13:09:55 +0300

    resolved deps in other poms

--- gateway/src/main/java/ru/dayneko/gateway/config/Config.java
+++ gateway/src/main/java/ru/dayneko/gateway/config/Config.java
@@ -1,4 +1,22 @@
 package ru.dayneko.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Config {
+
+    // todo draft // w8ing 4 bff servers
+    @Bean
+    public RouteLocator gatewayRoutesConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/api/v1/product/bff/**")
+                        .filters(f -> f.addRequestHeader("proxy-applied", "true")
+                                .redirect(302, "/product/bff"))
+                        .uri("http://bff-server/")
+                        .id("product-bff"))
+                .build();
+    }
 }
