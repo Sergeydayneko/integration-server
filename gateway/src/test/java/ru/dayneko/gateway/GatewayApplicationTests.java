7ff179fd529418f2cf65bcb87722a784e651446e
sdayneko <sergeydayneko@mail.ru>
2020-03-12 10:09:26 +0300

    init gateway eureka client

---
+++ gateway/src/test/java/ru/dayneko/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dayneko.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
